d6f65c471f1410e88ed07e74e3bebd7bdf5ed57f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2023-07-30 20:23:32 +0600

    :dizzy: fixed next-auth bug

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,16 +2,12 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  pages: {
-    signIn: "https://pc-builder-lovat.vercel.app/api/auth/signin",
-  },
 };
 
 export default NextAuth(authOptions);
