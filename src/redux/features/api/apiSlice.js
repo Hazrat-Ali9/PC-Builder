9409b65be134a04759c871c90b856a22adbda832
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-16 21:01:32 +0600

    api slice 🐳

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { HYDRATE } from "next-redux-wrapper";
-
+// api slice
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
